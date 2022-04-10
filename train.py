54167acc2d348ded52290c4fb8e08581fb2e86c8
pitter-patterz <102317297+pitter-patterz@users.noreply.github.com>
2022-04-10 17:29:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -32,8 +32,10 @@ def train(h1,v,lam):
             m.sgd_step(x0,y0)
              
     return losses,accs,m
+
 np.random.seed(123)
 print('\n Start training...Press Enter if the results are not printed.')
+
 for v in [7e-3,8e-3,10e-3]:
     for lam in [1e-4,5e-4,10e-4]:
         for h1 in [32,64,128]:            
